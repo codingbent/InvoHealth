9d347e46f1f89501fcdcd8f654f48885fbcaada1
abhed agarwal <abhed.agl@gmail.com>
2025-09-11 02:09:17 +0530

    edit

--- src/components/PatientDetails.js
+++ src/components/PatientDetails.js
@@ -196,7 +196,7 @@ const handleUpdateAppt = async () => {
                 </div>
                 <div className="mb-3">
                   <label className="form-label">Number</label>
-                  <input type="text" className="form-control" name="number" value={patient.number} onChange={handleChange} />
+                  <input type="number" className="form-control" name="number" value={patient.number} onChange={handleChange} />
                 </div>
                 <div className="mb-3">
                   <label className="form-label">Age</label>
