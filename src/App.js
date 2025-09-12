c2aeb25e43e83564a8ee231c2c8d11d4885c8191
abhed agarwal <abhed.agl@gmail.com>
2025-09-12 12:27:02 +0530

    MINOR FIX

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
-import About from "./components/About";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Navbar from "./components/Navbar";
@@ -27,7 +26,6 @@ function App() {
                 <Alert alert={alert} />
                 <Routes>
                     <Route path="/" element={<Home showAlert={showAlert} />} />
-                    <Route path="/about" element={<About />} />
                     <Route path="/patient/:id" element={<PatientDetails />} />
                     <Route
                         path="/login"
