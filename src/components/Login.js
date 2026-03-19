fdfe9a150cad78cf18e9b9e7ee8f23e3cf1299e1
abhed agarwal <abhed.agl@gmail.com>
2026-03-19 14:50:35 +0530

    Login debug

--- src/components/Login.js
+++ src/components/Login.js
@@ -225,6 +225,7 @@ export default function Login(props) {
                         placeholder="Enter password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        required
                     />
                 </div>
 
